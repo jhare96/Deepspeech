634b933b873a654e3bd9596af4b9a06decdf40f9
Joshua Hare <josh@DESKTOP-DRMSLS5.localdomain>
2019-02-26 13:57:58 +0000

    fixed test error

---
+++ preprocess_timit_data.py
@@ -0,0 +1,42 @@
+import numpy as np
+import timit_load_data
+import tools
+import pickle
+
+
+def load_timit_data(filename):
+    audiopath_full = "timit/"+str(filename)+".wav"
+    phonemepath_full = "timit/"+str(filename)+".phn"
+    #print(audiopath_full)
+    audio, text = timit_load_data.get_data(audiopath_full , phonemepath_full)
+    audio, text = tools.window_data(audio,text)
+    #print("text", text)
+    text = tools.text_to_onehot(text,len(timit_load_data.phones))
+    #print("text reconstructed", tools.onehot_to_text(text))
+    return audio,text 
+
+timit_filepaths = list(sorted(set(open("timit/allfilelist.txt", 'r').read().split("\n"))))[1:-1]
+print("number of filepaths", len(timit_filepaths))
+windowed_audio_data_list = []
+windowed_text_list = []
+for i in range(len(timit_filepaths)):
+      audio_data, text = load_timit_data(timit_filepaths[i])
+      windowed_audio_data_list.append(audio_data)
+      windowed_text_list.append(text)
+
+
+with open(r"processed_timit_audio.pickle", "wb") as output_file:
+   pickle.dump(windowed_audio_data_list, output_file)
+
+with open(r"processed_timit_phns.pickle", "wb") as output_file:
+   pickle.dump(windowed_text_list, output_file)
+
+with open(r"processed_timit_audio.pickle", "rb") as input_file:
+   e = pickle.load(input_file)
+
+with open(r"processed_timit_phns.pickle", "rb") as input_file:
+   f = pickle.load(input_file)
+
+print("len e", len(e))
+print("len f", len(f))
+print("number of audio files processed", len(windowed_audio_data_list))
